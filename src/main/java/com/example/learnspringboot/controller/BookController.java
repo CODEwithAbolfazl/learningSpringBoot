7b5c0eaeab4396ba1cf1e3a81e764ed6bd0e3ca4
abolfazl <abolfazl.noruzmahani@gmail.com>
2026-09-12 16:02:34 +0330

    rewrited the code to handle the BookDTO class for transforming data .

--- src/main/java/com/example/learnspringboot/controller/BookController.java
+++ src/main/java/com/example/learnspringboot/controller/BookController.java
@@ -1,11 +1,14 @@
 package com.example.learnspringboot.controller;
 
+import com.example.learnspringboot.dto.BookDTO;
 import com.example.learnspringboot.modes.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.learnspringboot.services.BookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -16,13 +19,13 @@ public class BookController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBook (@PathVariable long id){
+    public ResponseEntity<BookDTO> getBook (@PathVariable long id){
         return  ResponseEntity.ok(bookService.getBook(id));
     }
 
 
     @GetMapping
-    public ResponseEntity<Book[]> getBooks (){
+    public ResponseEntity<List<BookDTO>> getBooks (){
         return ResponseEntity.ok(bookService.getAllBooks()) ;
     }
 
@@ -33,14 +36,14 @@ public class BookController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> addBook (@RequestBody Book book){
-        bookService.addBook(book);
+    public ResponseEntity<Void> addBook (@RequestBody BookDTO bookDTO){
+        bookService.addBook(bookDTO);
         return new ResponseEntity<>(null , HttpStatus.CREATED);
     }
 
     @PutMapping
-    public  ResponseEntity<Void> updateBook ( @RequestBody Book book){
-        bookService.updateBook(book);
+    public  ResponseEntity<Void> updateBook ( @RequestBody BookDTO bookDTO){
+        bookService.updateBook(bookDTO);
         return new ResponseEntity<>(null , HttpStatus.NO_CONTENT);
     }
 
--- src/main/java/com/example/learnspringboot/services/BookService.java
+++ src/main/java/com/example/learnspringboot/services/BookService.java
@@ -1,19 +1,31 @@
 package com.example.learnspringboot.services;
 
+import aj.org.objectweb.asm.commons.Remapper;
+import com.example.learnspringboot.dto.BookDTO;
 import com.example.learnspringboot.globalExeptions.BookIDNotFoundException;
 import com.example.learnspringboot.modes.Book;
 import com.example.learnspringboot.repo.BookRepo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
 
  @Autowired
  private BookRepo repository ;
+
+    @Autowired
+  private ModelMapper modelMapper ;
  
- 
-    public  void addBook (Book book){
+    public  void addBook (BookDTO bookDTO){
+        if(bookDTO.getId() <= 0 || bookDTO.getAuthor() == null || bookDTO.getPrice() <= 0){
+            throw new BookIDNotFoundException(bookDTO.getId());
+        }
+
+        Book book = modelMapper.map(bookDTO ,Book.class);
         repository.save(book);
     }
 
@@ -25,21 +37,27 @@ public class BookService {
 
     }
 
-    public  void updateBook (Book book){
-        if(repository.existsById(book.getId())){
-            repository.save( book);
+    public  void updateBook (BookDTO bookDTO){
+        if(repository.existsById(bookDTO.getId())){
+            repository.save(modelMapper.map(bookDTO , Book.class));
         }else{
-            throw new BookIDNotFoundException(book.getId());
+            throw new BookIDNotFoundException(bookDTO.getId());
         }
 
     }
 
-    public Book getBook (long id){
-        return repository.findById(id).orElse(null);
+    public BookDTO getBook (long id){
+
+        Book book = repository.findById(id).orElseThrow(() -> new BookIDNotFoundException(id));
+        return (modelMapper.map(book , BookDTO.class));
     }
 
-    public Book [] getAllBooks (){
-        return (repository.findAll().toArray(Book[]::new));
+    public List <BookDTO> getAllBooks (){
+
+      return repository.findAll().stream()
+              .map(book -> modelMapper.map(book , BookDTO.class) )
+              .toList();
+
     }
 
 
